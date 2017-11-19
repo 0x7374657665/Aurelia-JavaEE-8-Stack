0150e187aac275d981ddc7b3136c4443142a5404
Steve Russell <0x7374657665@gmail.com>
2017-11-19 00:54:35 -0500

    modified base url environment property and added CORS filter to backend to enable separated frontend/backend development

--- frontend/aurelia_project/environments/prod.js
+++ frontend/aurelia_project/environments/prod.js
@@ -1,5 +1,5 @@
 export default {
   debug: false,
   testing: false,
-  host: 'base/'
+  host: 'api/'
 };
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,20 @@
 import environment from './environment'
+import {HttpClient} from 'aurelia-http-client'
 
 export class App {
+
   constructor() {
-    this.message = 'Hello World!';
-    console.log(environment.host)
+    this.client = new HttpClient().configure( conf => {
+      conf.withBaseUrl(environment.host)
+    })
+
+    this.message = 'Loading...'
+  }
+
+  activate() {
+    this.client.get('hello').then(data => {
+      const payload = JSON.parse(data.response)
+      this.message = payload.message
+    })
   }
 }
