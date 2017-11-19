25c5d812b9f0be545f726ea2f023fcd2f4617bad
Steve Russell <0x7374657665@gmail.com>
2017-11-19 10:28:18 -0500

    changed enums to be stored as text in database

--- backend/src/main/java/us/stevenrussell/base/books/Book.java
+++ backend/src/main/java/us/stevenrussell/base/books/Book.java
@@ -1,8 +1,6 @@
 package us.stevenrussell.base.books;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -14,9 +12,14 @@ public class Book {
     @Id
     @GeneratedValue
     private long id;
+
     private String title;
+
     private String description;
+    
     private int rating;
+
+    @Enumerated(EnumType.STRING)
     private BookStatus status;
 
     public Book(String title, String description, int rating, BookStatus status) {
